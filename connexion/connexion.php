8d15af10db9c3dc951fb3c780bca1c6d6e7d0fb9
Amadouloulou <amadou.niang@lepoles.com>
2017-07-24 03:22:12 +0200

    maj

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -1,9 +1,9 @@
 <?php
 
 $hote='localhost'; // Le chemin vers le serveur
-$bdd='aniang_bd'; // le nom de la base de données
-$utilisateur='amadou'; // le nom d'utilisateur pour se connecter
-$passe='Db53rr$3'; // mot de passe de l'utilisateur
+$bdd='sitecv_aniang'; // le nom de la base de données
+$utilisateur=''; // le nom d'utilisateur pour se connecter
+$passe=''; // mot de passe de l'utilisateur
 
 $pdoCV = new PDO('mysql:host='.$hote.';dbname='.$bdd, $utilisateur, $passe);
 //$pdoCV est le npom de la variable de la connexionqui sert partout ou l'on doit se servir de cette connexion.
